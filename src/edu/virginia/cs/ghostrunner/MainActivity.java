0a38914ca17a939f4a026f40dd883279e0587b9b
Maksud Sharif <ms4vc@virginia.edu>
2014-11-17 05:47:41 -0500

    Cleanup

--- src/edu/virginia/cs/ghostrunner/MainActivity.java
+++ src/edu/virginia/cs/ghostrunner/MainActivity.java
@@ -1,15 +1,10 @@
 package edu.virginia.cs.ghostrunner;
 
-import android.R.color;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.widget.Button;
 
